a9b9c94299bb828b10bdae4693cdae065cbb51aa
SpectreN117 <37374059+SpectreN117@users.noreply.github.com>
2018-03-15 08:44:14 -0400

    Updated from Brython Server: 3/15/2018 8:44:14 AM

--- rocket1.py
+++ rocket1.py
@@ -1,6 +1,16 @@
 from ggrocket import Rocket, Planet
-from math import radians
+from math import radians, sqrt
+from ggmath import Slider
 
-earth = Planet (viewscale=0.00005)
-rocket = Rocket (earth, heading = radians(90), directiond = 90, velocity=10)
+earth = Planet (viewscale = 0.00005)
+
+Re = 6.371E6
+Me = 5.972E24
+G = 6.674E-11
+
+Ve=sqrt(2*Me*G/Re)
+
+tz = Slider ((10, 400) , 0, 5, 0, positioning ="physical")
+
+rocket = Rocket (earth, heading = radians(90), directiond = 90, velocity=Ve, timezoom = tz)
 earth.run (rocket)
